1e58b940082a240ac2b72bbbedf86769a329f769
Jeovanni <jeovannildg@gmail.com>
2020-04-25 18:47:13 -0600

    completed getFullNames

--- index.js
+++ index.js
@@ -237,8 +237,12 @@ function stringSmash(strings) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const fullNames = [];
+  runners.forEach(element => {
+    fullNames.push(element.last_name + ", " + element.first_name);
+  })
+  return fullNames; 
 }
 
 /**
